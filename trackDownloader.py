f7faab0ff9603d583344c8480251169f07665ee1
wholeheartedness <73874814+wholeheartedness@users.noreply.github.com>
2022-12-29 17:39:47 +0200

    Added support for special character name

--- trackDownloader.py
+++ trackDownloader.py
@@ -4,11 +4,14 @@ import os
 from urllib import request
 import os
 import json
+import urllib.parse
 
 darkgreen = "\033[32m"
 reset = "\033[00m"
+link = "\033[34m\033[04m"
 
 DOWNLOAD_URL = "https://tmnf.exchange/trackgbx/"
+RANDOM_URL = "https://tmnf.exchange/trackrandom/?"
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -20,11 +23,26 @@ print(reset)
 print(" ")
 
 directory = ""
+while True:
+    print(f"   Documentation: {link}https://sites.google.com/view/trackrandom{reset}")
+    print(f"   Format: {darkgreen}<parameter>=<value>{reset}")
+    param = input("   Parameter (leave blank to continue):")
+    if param == "":
+        break
+    else:
+        param, value = param.split("=")
+        value = urllib.parse.quote(value).replace("%20", "+")
+        RANDOM_URL += f"{param}={value}&"
+
+RANDOM_URL = RANDOM_URL[:-1]
+
 
 while not os.path.exists(directory):
     directory = input("   Folder of tracks (default: ./tracks): ").strip('"')
     if directory == "":
         directory = "tracks"
+        if not os.path.exists("tracks"):
+            os.makedirs("tracks")
 
 while True:
     try:
@@ -38,7 +56,7 @@ print(" ")
 i = 0
 while i < count:
 
-    url = "https://tmnf.exchange/trackrandom"
+    url = RANDOM_URL
 
     r = requests.get(url)
 
@@ -64,7 +82,6 @@ while i < count:
         with open(f"{directory}/links/TMX LINKS.txt", "a") as links:
             links.write(f"{trackName} : https://tmnf.exchange/trackshow/{trackid}\n")
     except:
-        i -= 1
         continue
 
     print(f"   ({i + 1}) {darkgreen}Found: {reset}{trackName} {darkgreen}by: {reset}{author}")
